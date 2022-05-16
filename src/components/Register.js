b137a4854c27036cf08787b1a16feed8a21cc6e8
Peetu Kuoppala <peetu.kuoppala@tuni.fi>
2022-05-16 17:46:49 +0300

    added unstyled login + register

---
+++ src/components/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  '/Register.css';
+class Register extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h1>Register</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Register;
